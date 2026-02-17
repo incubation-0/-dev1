86bea91a1c9f7f77583beedee9cb64d9f4a3394a
Order <incubation@tutamail.com>
2026-02-17 18:47:54 +0300

    Batarya_sicaklığı denetleme kodu girildi

--- odev.c
+++ odev.c
@@ -7,5 +7,15 @@ int main() {
     int fren_pedali = 1;
 
 
+    printf("Batarya Sıcaklığı giriniz: ");
+    scanf("%f",&batarya_sicakligi);
+    if(batarya_sicakligi >= 60.00){
+        printf("KRİTİK HATA: Motor Aşırı Isındı! Sürüş Engellendi");
+    } else if(batarya_sicakligi <= -100.00 || batarya_sicakligi >= 200.00){
+        printf("Olanaksız sıcaklık, hatalı sensör verisi");
+    } else{
+        
+    }
+
 }
 
