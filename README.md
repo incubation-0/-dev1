5f60d0c3b51e47a0dc788708753ad4f96a045298
Order <incubation@tutamail.com>
2026-02-18 20:56:02 +0300

    odev.c dosyasındaki soru sorma sırası art arda ve sıkıntıları sorular sorulduktan sonra tek tek yazmaya programlandı (Mantık hatalarında direk mantık hatası mesajı verilir ve geri kalan sorular sorulmaz)
    README.md dosyası odev.c dosyasındeki değişikliklere göre güncellenmiştir

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # -dev1
 Sırasıyla batarya sıcaklığıının, şarj yüzdesinin, kapı durumunun ve fren pedalinin değerlerini denetler ve girilen değerlere göre işin sonunda motor başlatılabilir mesajını verir. 
 
-Ama eğer girilen değerler istenilen aralıkda değilse başlatılamaz tarzı mesajlar verir.
-Ve mantık hatası değerler için özel hata mesajları vardır.
+Ama eğer girilen değerler istenilen aralıkda değilse her sıkıntıya özel başlatılamaz tarzı mesajlar verir.
+Son olarak mantık hatası değerler için özel hata mesajları vardır ve direk diğer soruları sormadan bitirir.
--- odev.c
+++ odev.c
@@ -6,44 +6,45 @@ int main() {
     char kapi_durumu = 'Z';
     int fren_pedali = 1;
 
-
-    printf("Batarya Sıcaklığı giriniz: ");
+    printf("Batarya sıcaklığını giriniz: ");
     scanf("%f",&batarya_sicakligi);
-    if(batarya_sicakligi >= 60.00){
-        printf("KRİTİK HATA: Motor Aşırı Isındı! Sürüş Engellendi\n");
-    } else if(batarya_sicakligi <= -100.00 || batarya_sicakligi >= 200.00){
+
+    if(batarya_sicakligi <= -100.00 || batarya_sicakligi >= 200.00){
         printf("Olanaksız sıcaklık, hatalı sensör verisi\n");
     } else{
-        printf("Şarj yüzdesi kaçtır: ");
-        scanf("%d",&sarj_yuzdesi);
-            if(sarj_yuzdesi < 0 || sarj_yuzdesi > 100){
-                printf("\n Hatalı sensör verisi");
-            } else if(sarj_yuzdesi < 10){
-                printf("UYARI: Batarya Kritik Seviyede! Sürüş Başlatılamaz.\n");
+            printf("Şarj yüzdesi kaçtır: ");
+            scanf("%d",&sarj_yuzdesi);
+            if (sarj_yuzdesi < 0 || sarj_yuzdesi > 100){
+                printf("Hatalı sensör verisi");
             } else{
-                      printf("Kapı durumu gir:(Açık için A, Kapalı için K): ");
-                      scanf("%s",&kapi_durumu );
-                      if(kapi_durumu != 'K' && kapi_durumu != 'A'){
-                         printf("\n GEÇERSİZ ÇAR. \n");
+                printf("Kapı durumu gir:(Açık için A, Kapalı için K): ");
+                scanf("%s",&kapi_durumu );
+                if(kapi_durumu != 'K' && kapi_durumu != 'A'){
+                printf("GEÇERSİZ ÇAR. \n");
+                } else{
+                    printf("fren_pedali durumu giriniz(1 basılı, 0 basılı değil): ");
+                    scanf("%d",&fren_pedali);
+                    if(fren_pedali != 0 || fren_pedali != 1){
+                    printf("GEÇERSİZ GİRDİ. \n");
                     } else{
-                        if(kapi_durumu == 'A'){
-                            printf("HATA: Kapılar Açık! Lütfen Kapatınız.");
-                        } else{
-                                printf("fren_pedali durumu giriniz(1 basılı, 0 basılı değil): ");
-                                scanf("%d",&fren_pedali);
-                                if(fren_pedali != 0 && fren_pedali != 1) {
-                                    printf("geçersiz girdi");
-                                } else{
-                                    if(fren_pedali == 0){
-                                        printf("BİLGİ: Güvenlik için frene basarak tekrar deneyin.");
-                                    } else{
-                                        printf("BAŞARILI: SİSTEM HAZIR. MOTOR BAŞLATILIYOR...");
-                                    }
-                                }
+                        if(batarya_sicakligi >= 60.00){
+                        printf("KRİTİK HATA: Motor Aşırı Isındı! Sürüş Engellendi\n");
                         }
-                    }
-            }
+                        if(sarj_yuzdesi < 10){
+                        printf("UYARI: Batarya Kritik Seviyede! Sürüş Başlatılamaz.\n");
+                        }
+                        if(kapi_durumu == 'A'){
+                        printf("HATA: Kapılar Açık! Lütfen Kapatınız. \n");
+                        }
+                        if(fren_pedali == 0){
+                        printf("BİLGİ: Güvenlik için frene basarak tekrar deneyin. \n");
+                        }
+                        if(batarya_sicakligi < 60.00 && sarj_yuzdesi >= 10 && kapi_durumu == 'K' && fren_pedali == 1){
+                        printf("Her şey hazır motor başlatılmakta");
+                        }
+                    } 
+                } 
+            }   
+        }
     }
-
-}
 
